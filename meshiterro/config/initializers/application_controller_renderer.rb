07f3b1d5aa7a68df2e780f3e91d49515949a75b3
tmy628 <tmy6h1r@gmail.com>
2021-03-22 08:01:32 +0000

    [Fix] 5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bff44451af1ee84b1033a26696b96c0429439ce71458d3b25a4bf7b13de9556fd6fbf72026d117620a7db9d040e75caf76048ad9c4d044f9b7ce5925e3c40a7b'
--- meshiterro/db/schema.rb
+++ meshiterro/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_061723) do
+ActiveRecord::Schema.define(version: 2021_03_22_063317) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
